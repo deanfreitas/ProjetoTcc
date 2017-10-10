2f92ebf6199d11c93989ee600e64c2604b42b873
gilberto.a.freitas <Gilb!789456123>
2017-10-10 18:32:02 -0300

    refactor code

--- src/main/webapp/resources/JavaScript/validarTelas/validarPaciente.js
+++ src/main/webapp/resources/JavaScript/validarTelas/validarPaciente.js
@@ -161,6 +161,11 @@ $(document).ready(function () {
         });
     }
 
+    /**
+     * Precisa errumar essa função pois, quando aperta o botão "Gerar relatório".
+     * Esta pegando apenas o primero paciente da lista, não o paciente da flag selecionada
+     * @param fields
+     */
     function gerarRelatorio(fields) {
         fields.click(function () {
             if (listIdPaciente.length === 1) {
@@ -173,6 +178,11 @@ $(document).ready(function () {
         });
     }
 
+    /**
+     * Precisa errumar essa função pois, quando aperta o botão "acompanhamento".
+     * Esta pegando apenas o primero paciente da lista, não o paciente da flag selecionada
+     * @param fields
+     */
     function telaAcompanhamento(fields) {
         fields.click(function () {
             if (listIdPaciente.length === 1) {
