28b48323fa79f8b4be542f047fc0f6bba7e18ace
Gilberto Freitas <gilberto.ademar@gmail.com>
2018-10-18 00:48:47 -0300

    refactor code

--- src/main/webapp/resources/Sql/InsertTabelasTeste.sql
+++ src/main/webapp/resources/Sql/InsertTabelasTeste.sql
@@ -1,46 +1,53 @@
 USE projetotcc;
 
-INSERT INTO login (Log_Usuario, Log_Senha) VALUES ('administrador', 12345);
-INSERT INTO nutricionista (Nutri_NomeCompleto, Nutri_Crn, Id_Login) VALUES ('teste', 12345, 1);
-INSERT INTO role (Role_Name, id_nutricionista) VALUES ('ROLE_administrador', 1);
+INSERT INTO Login (Log_Usuario, Log_Senha)
+VALUES ('administrador', 12345);
+INSERT INTO Nutricionista (Nutri_NomeCompleto, Nutri_Crn, Id_Login)
+VALUES ('teste', 12345, 1);
+INSERT INTO Role (Role_Name, id_nutricionista)
+VALUES ('ROLE_administrador', 1);
 
-INSERT INTO identificacao (Ident_Nome, Ident_Idade, Ident_Sexo) VALUES ('Marcia', 22, 'Feminino');
-INSERT INTO login (Log_Usuario, Log_Senha) VALUES ('administrador1', 12345);
-INSERT INTO paciente (Id_identificacao, id_nutricionista, Id_Login) VALUES (1, 1, 2);
-INSERT INTO role (Role_Name, id_Paciente) VALUES ('ROLE_administrador', 1);
+INSERT INTO Identificacao (Ident_Nome, Ident_Idade, Ident_Sexo)
+VALUES ('Marcia', 22, 'Feminino');
+INSERT INTO Login (Log_Usuario, Log_Senha)
+VALUES ('administrador1', 12345);
+INSERT INTO Paciente (Id_identificacao, id_nutricionista, Id_Login)
+VALUES (1, 1, 2);
+INSERT INTO Role (Role_Name, id_Paciente)
+VALUES ('ROLE_administrador', 1);
 
-INSERT INTO ceia (Ceia_Horario, Ceia_Local, Ceia_Humor, Ceia_Alimento, Ceia_Quantidade)
+INSERT INTO Ceia (Ceia_Horario, Ceia_Local, Ceia_Humor, Ceia_Alimento, Ceia_Quantidade)
 VALUES ('8:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO colacao (Colac_Horario, Colac_Local, Colac_Humor, Colac_Alimento, Colac_Quantidade)
+INSERT INTO Colacao (Colac_Horario, Colac_Local, Colac_Humor, Colac_Alimento, Colac_Quantidade)
 VALUES ('9:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO jantar (Jant_Horario, Jant_Local, Jant_Humor, Jant_Alimento, Jant_Quantidade)
+INSERT INTO Jantar (Jant_Horario, Jant_Local, Jant_Humor, Jant_Alimento, Jant_Quantidade)
 VALUES ('10:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO lanche (Lan_Horario, Lan_Local, Lan_Humor, Lan_Alimento, Lan_Quantidade)
+INSERT INTO Lanche (Lan_Horario, Lan_Local, Lan_Humor, Lan_Alimento, Lan_Quantidade)
 VALUES ('12:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO almoco (Alm_Horario, Alm_Local, Alm_Humor, Alm_Alimento, Alm_Quantidade)
+INSERT INTO Almoco (Alm_Horario, Alm_Local, Alm_Humor, Alm_Alimento, Alm_Quantidade)
 VALUES ('15:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO desjejum (Desj_Horario, Desj_Local, Desj_Humor, Desj_Alimento, Desj_Quantidade)
+INSERT INTO Desjejum (Desj_Horario, Desj_Local, Desj_Humor, Desj_Alimento, Desj_Quantidade)
 VALUES ('18:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO fora_de_hora (For_Horario, For_Local, For_Humor, For_Alimento, For_Quantidade)
+INSERT INTO Fora_de_Hora (For_Horario, For_Local, For_Humor, For_Alimento, For_Quantidade)
 VALUES ('20:30', 'Test', 'Feliz', 'Arroz', '1');
-INSERT INTO data (d_data, Id_Ceia, Id_Colacao, Id_Jant, Id_Lanche, Id_Almoco, Id_Desj, Id_Foradehora, id_Paciente)
+INSERT INTO Data (d_data, Id_Ceia, Id_Colacao, Id_Jant, Id_Lanche, Id_Almoco, Id_Desj, Id_Foradehora, id_Paciente)
 VALUES ('20171128', 1, 1, 1, 1, 1, 1, 1, 1);
 
-INSERT INTO ceia (Ceia_Horario, Ceia_Local, Ceia_Humor, Ceia_Alimento, Ceia_Quantidade)
+INSERT INTO Ceia (Ceia_Horario, Ceia_Local, Ceia_Humor, Ceia_Alimento, Ceia_Quantidade)
 VALUES ('8:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO colacao (Colac_Horario, Colac_Local, Colac_Humor, Colac_Alimento, Colac_Quantidade)
+INSERT INTO Colacao (Colac_Horario, Colac_Local, Colac_Humor, Colac_Alimento, Colac_Quantidade)
 VALUES ('9:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO jantar (Jant_Horario, Jant_Local, Jant_Humor, Jant_Alimento, Jant_Quantidade)
+INSERT INTO Jantar (Jant_Horario, Jant_Local, Jant_Humor, Jant_Alimento, Jant_Quantidade)
 VALUES ('10:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO lanche (Lan_Horario, Lan_Local, Lan_Humor, Lan_Alimento, Lan_Quantidade)
+INSERT INTO Lanche (Lan_Horario, Lan_Local, Lan_Humor, Lan_Alimento, Lan_Quantidade)
 VALUES ('12:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO almoco (Alm_Horario, Alm_Local, Alm_Humor, Alm_Alimento, Alm_Quantidade)
+INSERT INTO Almoco (Alm_Horario, Alm_Local, Alm_Humor, Alm_Alimento, Alm_Quantidade)
 VALUES ('15:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO desjejum (Desj_Horario, Desj_Local, Desj_Humor, Desj_Alimento, Desj_Quantidade)
+INSERT INTO Desjejum (Desj_Horario, Desj_Local, Desj_Humor, Desj_Alimento, Desj_Quantidade)
 VALUES ('18:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO fora_de_hora (For_Horario, For_Local, For_Humor, For_Alimento, For_Quantidade)
+INSERT INTO Fora_de_Hora (For_Horario, For_Local, For_Humor, For_Alimento, For_Quantidade)
 VALUES ('20:30', 'Test1', 'Triste', 'Arroz', '2');
-INSERT INTO data (d_data, Id_Ceia, Id_Colacao, Id_Jant, Id_Lanche, Id_Almoco, Id_Desj, Id_Foradehora, id_Paciente)
+INSERT INTO Data (d_data, Id_Ceia, Id_Colacao, Id_Jant, Id_Lanche, Id_Almoco, Id_Desj, Id_Foradehora, id_Paciente)
 VALUES ('20171120', 2, 2, 2, 2, 2, 2, 2, 1);
 
 INSERT INTO Exames_Bioquimicos (id_Paciente, Bio_Data, Bio_Vlrderefe, Bio_GLI, Bio_Hg, Bio_Tg, Bio_Ct, Bio_Ldl, Bio_Hdl, Bio_Sodio, Bio_Potassio, Bio_Creatina, Bio_Tgo, Bio_Tgp, Bio_GGT, Bio_Ferritina, Bio_Vitaminab12, Bio_Acidofolico, Bio_Naohdl, Bio_Vldl, Bio_Ureia, Bio_Fosforo, Bio_Calcio, Bio_Acidourico)
